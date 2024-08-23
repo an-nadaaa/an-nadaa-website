cbaa4ea31386aeb9347077c9fc36678041086f9a
fazaamal <fazasophian123@gmail.com>
2024-08-23 20:16:06 +0800

    feat: first about page cm, intro, impact and some history section

---
+++ components/about/StorySection.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-light-blue py-16">
+    <div class="container">
+      <div class="w-[600px] max-w-full">
+        <h6 class="text-xs font-medium text-orange-accent">HOW IT STARTS</h6>
+        <h1 class="font-medium my-4">We are just getting started</h1>
+        <p class="text-dark-gray font-light">
+          An-Nadaa started with the renovation of a single school building in
+          2014, since then it has grown into an organisation that has
+          established Awqaaf and executed many projects and is building the
+          community through education and welfare causes. An-Nadaa is an
+          officially registered Non-Profit Organisation by the Malaysian and
+          Nigerian governments.
+
+          <br /><br />Our causes aim to build self-sustaining communities in
+          which people who were once in need of support transform into someone
+          who supports and uplifts others. Although we started off in Zaria city
+          in Nigeria our vision is to cover the whole country and one day reach
+          the whole of Africa.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style></style>
