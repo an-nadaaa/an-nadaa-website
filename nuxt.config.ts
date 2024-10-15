46fe909b725a5c16e5f6aea5a16296da8ca67862
fazaamal <fazasophian123@gmail.com>
2024-10-15 15:38:57 +0800

    feat: cause page to fetch from strapi backend

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,28 @@ export default defineNuxtConfig({
   },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      functionBaseUrl:
+        process.env.NODE_ENV === "production"
+          ? `${process.env.BASE_URL_PROD}/.netlify/functions`
+          : `${process.env.BASE_URL_DEV}/.netlify/functions`,
+      lang: "en-US",
+      STRIPE_PK:
+        process.env.NODE_ENV === "production"
+          ? process.env.STRIPE_PK_PROD
+          : process.env.STRIPE_PK_DEV,
+      STRAPI_API:
+        process.env.NODE_ENV === "production"
+          ? process.env.STRAPI_API_PROD
+          : process.env.STRAPI_API_DEV,
+      STRAPI_API_KEY:
+        process.env.NODE_ENV === "production"
+          ? process.env.STRAPI_API_KEY_PROD
+          : process.env.STRAPI_API_KEY_DEV,
+    },
+  },
+
   modules: [
     "@nuxtjs/tailwindcss",
     "@vueuse/nuxt",
@@ -56,4 +78,4 @@ export default defineNuxtConfig({
     lazy: true,
     strategy: "prefix_except_default",
   },
-});
+})
