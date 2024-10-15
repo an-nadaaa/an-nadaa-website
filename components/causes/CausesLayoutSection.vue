9bae57862cc32acc16192e0fa29ca6da2e0a337d
fazaamal <fazasophian123@gmail.com>
2024-10-15 23:53:16 +0800

    feat: causes page fetch from strapi backend and filtering

--- components/causes/CausesLayoutSection.vue
+++ components/causes/CausesLayoutSection.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container py-6 sm:py-12">
+    <!-- <h1>{{ categories }}</h1> -->
     <div class="grid sm:grid-cols-4 gap-x-3 gap-y-3">
       <div class="">
         <p class="select-label">Category</p>
@@ -13,7 +14,7 @@
               <SelectItem
                 v-for="(category, index) in categories"
                 :key="index"
-                :value="category.toLowerCase()"
+                :value="category"
                 >{{ category }}</SelectItem
               >
             </SelectGroup>
@@ -32,7 +33,7 @@
               <SelectItem
                 v-for="(state, index) in states"
                 :key="index"
-                :value="state.toLowerCase()"
+                :value="state"
                 >{{ state }}</SelectItem
               >
             </SelectGroup>
@@ -51,7 +52,7 @@
               <SelectItem
                 v-for="(location, index) in locations"
                 :key="index"
-                :value="location.toLowerCase()"
+                :value="location"
                 >{{ location }}</SelectItem
               >
             </SelectGroup>
@@ -69,25 +70,26 @@
     </div>
     <div class="grid gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-3">
       <NuxtLink
-        v-for="(campaign, index) in campaigns"
+        v-for="(cause, index) in causes"
         :id="index"
-        :to="'/causes/' + campaign.id"
+        :to="'/causes/' + cause.documentId"
       >
         <HomeCampaignCardSection
-          :key="campaign.id"
-          :title="campaign.title"
-          :categoryTags="campaign.categoryTags"
-          :image="campaign.image"
-          :amountRaised="campaign.amountRaised"
-          :goalAmount="campaign.goalAmount"
-          :status="campaign.status"
-          :funded="campaign.funded"
-          :tags="campaign.tags"
+          :key="cause.documentId"
+          :title="cause.title"
+          :categoryTags="cause.categories.map((category:any) => category.title)"
+          :image="cause.images[0].formats.medium.url"
+          :amountRaised="cause.raisedAmount"
+          :goalAmount="cause.goalDetails[0].goalAmount"
+          :status="cause.causeStatus"
+          :funded="false"
+          :tags="cause.tags.map((tag:any) => tag.value)"
           class="hover:cursor-pointer hover:shadow-xl"
         />
       </NuxtLink>
     </div>
   </div>
+  <!-- <h1>{{ causes }}</h1> -->
 </template>
 
 <script setup lang="ts">
@@ -101,80 +103,138 @@ import {
   SelectValue,
 } from "~/components/ui/select"
 
-const router = useRouter()
-const categories = ["All", "Education", "Food"]
+const pageSize = 12
+const currentIndex = ref(0)
+const strapiFetch = useStrapiFetch()
+const categories = await strapiFetch("/categories", "GET", {}).then((res) => {
+  return ["All", ...res.data.value.data.map((category: any) => category.title)]
+})
+const locations = await strapiFetch("/locations", "GET", {}).then((res) => {
+  return [
+    "All",
+    ...res.data.value.data.map((location: any) => location.countryName),
+  ]
+})
+// const categories = ["All", "Education", "Food"]
 const states = ["All", "Ongoing", "Funded"]
-const locations = ["All", "Nigeria", "Malaysia"]
+// const locations = ["All", "Nigeria", "Malaysia"]
 
-const categorySelected = ref("all")
-const stateSelected = ref("all")
-const locationSelected = ref("all")
+const categorySelected = ref("All")
+const stateSelected = ref("All")
+const locationSelected = ref("All")
 
 function handleResetFilter() {
-  categorySelected.value = "all"
-  stateSelected.value = "all"
-  locationSelected.value = "all"
+  categorySelected.value = "All"
+  stateSelected.value = "All"
+  locationSelected.value = "All"
 }
 
-const campaigns = [
-  {
-    id: 1,
-    title: "Sponsor a Child Education",
-    categoryTags: ["Education", "Sadaqah"],
-    image:
-      "https://wallpapers.com/images/hd/colorful-google-polka-dots-ot2t9bbzehsjggdx.jpg",
-    amountRaised: 4271,
-    goalAmount: 10650,
-    status: "Ongoing",
-    funded: false,
-    tags: ["20 days left"],
-  },
-  {
-    id: 2,
-    title: "Build a Masjid",
-    categoryTags: ["Sadaqah Jariyah"],
-    image:
-      "https://mrwallpaper.com/images/hd/vintage-google-website-7qgz2szwjhkoybl2.jpg",
-    amountRaised: 400,
-    goalAmount: 4271,
-    status: "Funded",
-    funded: true,
-  },
-  {
-    id: 2,
-    title: "Build a Masjid",
-    categoryTags: ["Sadaqah Jariyah"],
-    image:
-      "https://mrwallpaper.com/images/hd/vintage-google-website-7qgz2szwjhkoybl2.jpg",
-    amountRaised: 1700,
-    goalAmount: 4271,
-    status: "Funded",
-    funded: true,
-  },
-  {
-    id: 2,
-    title: "Build a Masjid",
-    categoryTags: ["Sadaqah Jariyah"],
-    image:
-      "https://mrwallpaper.com/images/hd/vintage-google-website-7qgz2szwjhkoybl2.jpg",
-    amountRaised: 2000,
-    goalAmount: 4271,
-    status: "Ongoing",
-    funded: true,
-  },
-  {
-    id: 2,
-    title: "Build a Masjid",
-    categoryTags: ["Sadaqah Jariyah"],
-    image:
-      "https://mrwallpaper.com/images/hd/vintage-google-website-7qgz2szwjhkoybl2.jpg",
-    amountRaised: 4271,
-    goalAmount: 4271,
-    status: "Funded",
-    funded: true,
-  },
-  // Add more campaigns as needed
-]
+const causes = computedAsync(async () => {
+  const qsQuery = {
+    filters: {
+      // category: {
+      //   $eq: categorySelected.value === "All" ? null : categorySelected.value,
+      // },
+      categories: {
+        title:
+          categorySelected.value === "All"
+            ? {}
+            : {
+                $eq: categorySelected.value,
+              },
+      },
+      causeStatus:
+        stateSelected.value === "All"
+          ? {}
+          : {
+              $eq: stateSelected.value,
+            },
+      locations: {
+        countryName:
+          locationSelected.value === "All"
+            ? {}
+            : {
+                $eq: locationSelected.value,
+              },
+      },
+    },
+    pagination: {
+      start: currentIndex.value,
+      limit: pageSize,
+    },
+    populate: "*",
+  }
+  const { data: strapiResponse } = await strapiFetch(
+    "/causes",
+    "GET",
+    {
+      locale: "en",
+    },
+    qsQuery
+  )
+
+  return strapiResponse.value.data
+})
+
+// const causes = [
+//   {
+//     id: 1,
+//     title: "Sponsor a Child Education",
+//     categoryTags: ["Education", "Sadaqah"],
+//     image:
+//       "https://wallpapers.com/images/hd/colorful-google-polka-dots-ot2t9bbzehsjggdx.jpg",
+//     amountRaised: 4271,
+//     goalAmount: 10650,
+//     status: "Ongoing",
+//     funded: false,
+//     tags: ["20 days left"],
+//   },
+//   {
+//     id: 2,
+//     title: "Build a Masjid",
+//     categoryTags: ["Sadaqah Jariyah"],
+//     image:
+//       "https://mrwallpaper.com/images/hd/vintage-google-website-7qgz2szwjhkoybl2.jpg",
+//     amountRaised: 400,
+//     goalAmount: 4271,
+//     status: "Funded",
+//     funded: true,
+//   },
+//   {
+//     id: 2,
+//     title: "Build a Masjid",
+//     categoryTags: ["Sadaqah Jariyah"],
+//     image:
+//       "https://mrwallpaper.com/images/hd/vintage-google-website-7qgz2szwjhkoybl2.jpg",
+//     amountRaised: 1700,
+//     goalAmount: 4271,
+//     status: "Funded",
+//     funded: true,
+//   },
+//   {
+//     id: 2,
+//     title: "Build a Masjid",
+//     categoryTags: ["Sadaqah Jariyah"],
+//     image:
+//       "https://mrwallpaper.com/images/hd/vintage-google-website-7qgz2szwjhkoybl2.jpg",
+//     amountRaised: 2000,
+//     goalAmount: 4271,
+//     status: "Ongoing",
+//     funded: true,
+//   },
+//   {
+//     id: 2,
+//     title: "Build a Masjid",
+//     categoryTags: ["Sadaqah Jariyah"],
+//     image:
+//       "https://mrwallpaper.com/images/hd/vintage-google-website-7qgz2szwjhkoybl2.jpg",
+//     amountRaised: 4271,
+//     goalAmount: 4271,
+//     status: "Funded",
+//     funded: true,
+//   },
+//   // Add more campaigns as needed
+// ]
 </script>
 
 <style>
