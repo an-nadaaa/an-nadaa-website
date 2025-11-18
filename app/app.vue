f01f48e23140bdca12de515e690da61b8797beec
fazaamal <fazasophian123@gmail.com>
2025-11-18 13:10:20 +0800

    refactor: remove user session display from app.vue for cleaner UI; retain essential components

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,4 @@
 <template>
-  <div class="fixed right-0 bottom-0 bg-white">
-    <p>is logged in {{ loggedIn }}</p>
-    <p>user {{ user?.user?.id }}</p>
-    <p>token {{ user?.token?.slice(0, 20) }}...</p>
-  </div>
-
   <Toaster />
   <NuxtLoadingIndicator />
   <NuxtLayout>
@@ -14,8 +8,6 @@
 
 <script setup lang="ts">
 const appConfig = useAppConfig()
-
-const { loggedIn, user } = useUserSession()
 
 useHead({
   title: appConfig.siteName as string,
