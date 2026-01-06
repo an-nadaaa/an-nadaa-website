6f50ba3a326acd5cfe69795b79baea0d7cead040
fazaamal <fazasophian123@gmail.com>
2026-01-06 16:18:42 +0800

    fix: enhance button component styling by adding hover cursor pointer class

--- app/components/ui/button/Button.vue
+++ app/components/ui/button/Button.vue
@@ -3,7 +3,7 @@ import type { PrimitiveProps } from "reka-ui"
 import type { HTMLAttributes } from "vue"
 import type { ButtonVariants } from "."
 import { Primitive } from "reka-ui"
-import { cn } from '~/lib/utils'
+import { cn } from "~/lib/utils"
 import { buttonVariants } from "."
 
 interface Props extends PrimitiveProps {
@@ -22,7 +22,9 @@ const props = withDefaults(defineProps<Props>(), {
     data-slot="button"
     :as="as"
     :as-child="asChild"
-    :class="cn(buttonVariants({ variant, size }), props.class)"
+    :class="
+      cn(buttonVariants({ variant, size }), props.class, 'hover:cursor-pointer')
+    "
   >
     <slot />
   </Primitive>
