80ca188b578ec7bbe38e0cc657ca88ef9d3c10bc
fazaamal <fazasophian123@gmail.com>
2026-02-10 20:11:36 +0900

    feat: add loading state to button component with spinner for better user feedback

--- app/components/ui/button/Button.vue
+++ app/components/ui/button/Button.vue
@@ -5,11 +5,13 @@ import type { ButtonVariants } from "."
 import { Primitive } from "reka-ui"
 import { cn } from "~/lib/utils"
 import { buttonVariants } from "."
+import { VueSpinner } from "vue3-spinners"
 
 interface Props extends PrimitiveProps {
   variant?: ButtonVariants["variant"]
   size?: ButtonVariants["size"]
   class?: HTMLAttributes["class"]
+  isLoading?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -20,12 +22,20 @@ const props = withDefaults(defineProps<Props>(), {
 <template>
   <Primitive
     data-slot="button"
+    :disabled="isLoading"
     :as="as"
     :as-child="asChild"
     :class="
-      cn(buttonVariants({ variant, size }), props.class, 'hover:cursor-pointer')
+      cn(buttonVariants({ variant, size }), props.class, {
+        'hover:cursor-pointer': !isLoading,
+      })
     "
   >
-    <slot />
+    <template v-if="isLoading">
+      <VueSpinner class="text-white" />
+    </template>
+    <template v-else>
+      <slot />
+    </template>
   </Primitive>
 </template>
